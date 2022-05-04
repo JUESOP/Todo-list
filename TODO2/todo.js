1cbe65b76117c67e723938650b712c75392e062b
JUESOP <sinangun@naver.com>
2022-05-04 09:30:07 +0900

    할 일 삭제, 수정, 완료 처리,

--- TODO2/todo.js
+++ TODO2/todo.js
@@ -26,18 +26,23 @@ const paintTodos = () => {
 
       // "todo-item"에 해당하는 HTML을 그려서 "todo-list"에 추가하기
       allTodos.forEach(todo => {
-          const todoItem= document.createElement('li');
+          const todoItem = document.createElement('li');
           todoItem.classList.add('todo-item');
 
           const checkedBox = document.createElement('div');
           checkedBox.classList.add('checkbox');
+          checkedBox.addEventListener('click', () => completeTodo(todo.id));
 
           const todoEle = document.createElement('div');
           todoEle.classList.add('todo');
+          todoEle.addEventListener('dblclick', (event) => onDblcickTodo(event, todo.id))
           todoEle.innerText = todo.content;
 
           const delButton = document.createElement('button');
           delButton.classList.add('delBtn');
+          delButton.addEventListener('click', () => {
+              deleteTodo(todo.id)
+          })
           delButton.innerHTML = 'X';
 
           if(todo.isCompleted) {
@@ -51,6 +56,53 @@ const paintTodos = () => {
 
           todoList.appendChild(todoItem);
       })
+}
+
+const deleteTodo = (todoId) => {
+    const newTodos = getAllTodos().filter(todo => todo.id !== todoId); //주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 반환
+    setTodos(newTodos);
+    paintTodos();      
+}
+
+const completeTodo = (todoId) => {
+    const newTodos = getAllTodos().map(todo => todo.id === todoId ? {...todo, isCompleted: !todo.isCompleted} : todo);
+    setTodos(newTodos);
+    paintTodos();
+}
+
+const onDblcickTodo = (e, todoId) => {
+    const todoEle = e.target;
+    const inputText = e.target.innerText;
+    const todoItemEle = todoEle.parentNode;
+    const inputEle = document.createElement('input');
+    inputEle.value = inputText;
+    inputEle.classList.add('edit-input');
+
+    inputEle.addEventListener('keydown', (e) => {
+      if(e.key === 'Enter') {
+          updateTodo(e.target.value, todoId);
+          document.body.removeEventListener('click', onClickBody);
+      }  
+    })
+
+     // todoItemEle 요소를 제외한 영역을 클릭 시, 수정모드 종료
+     const onClickBody = (e) => {
+        if(e.target !== inputEle) {
+            todoItemEle.removeChild(inputEle);
+            document.body.removeEventListener('click', onClickBody);
+        }
+    }
+
+    // body에 클릭에 대한 이벤트 리스너 등록
+    document.body.addEventListener('click', onClickBody);
+
+    todoItemEle.appendChild(inputEle);
+}
+
+const updateTodo = (text, todoId) => {
+    const newTodos = getAllTodos().map(todo => todo.id === todoId ? ({...todo, content: text}) : todo);
+    setTodos(newTodos);
+    paintTodos();
 }
 
 const init = () => {
